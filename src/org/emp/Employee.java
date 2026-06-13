ed1cceb0d3a5bf756089462db28c5a6d4f7d52fa
seraraja <seraraja1196@gmail.com>
2026-06-13 15:44:22 +0530

    first changes

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,16 @@
+package org.emp;
+
+public class Employee {
+
+	public void empId() {
+		System.out.println("Employee id is 10");
+	}
+
+	public static void main(String[] args) {
+		
+		Employee e=new Employee();
+		e.empId();
+		
+		
+	}
+}
