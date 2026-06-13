63082893e3f800b92dfdccfdd3087b8b1e61e969
seraraja <seraraja1196@gmail.com>
2026-06-13 16:02:03 +0530

    third changes

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -8,11 +8,14 @@ public class Employee {
 	public void empName() {
 		System.out.println("Employee name is sera");
 	}
-
+	public void empPhone() {
+		System.out.println("Employee phone number is 876876");
+	}
 	public static void main(String[] args) {
 		
 		Employee e=new Employee();
 		e.empId();		
 		e.empName();
+		e.empPhone();
 	}
 }
