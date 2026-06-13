9b08ee615053bc76f1b48acd4ac25fb88b85ecf2
seraraja1 <seraraja1196@gmail.com>
2026-06-13 15:54:48 +0530

    Update Employee

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -5,12 +5,14 @@ public class Employee {
 	public void empId() {
 		System.out.println("Employee id is 10");
 	}
+	public void empName() {
+		System.out.println("Employee name is sera");
+	}
 
 	public static void main(String[] args) {
 		
 		Employee e=new Employee();
-		e.empId();
-		
-		
+		e.empId();		
+		e.empName();
 	}
 }
